1ff7f5a354ead02bc5da15e507c3e083a27a508f
Leon Si <leon@leondreamed.com>
2023-09-01 09:11:47 -0400

    fix: bug

--- src/utils/bundle.ts
+++ src/utils/bundle.ts
@@ -4,18 +4,18 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import virtual from '@rollup/plugin-virtual';
 import commonjs from '@rollup/plugin-commonjs';
 
-export async function bundleNpmPackage(pkg: string) {
+export async function bundleNpmPackage(
+	pkg: string,
+	options?: { minify?: boolean }
+) {
 	const bundle = await rollup({
-		input: 'pkg',
+		input: 'pkg.cjs',
 		plugins: [
 			(virtual.default ?? virtual)({
-				pkg: `export * from ${JSON.stringify(pkg)}`
+				'pkg.cjs': `module.exports = require(${JSON.stringify(pkg)})`
 			}),
 			(esbuild.default ?? esbuild)({
-				loaders: {
-					'.ts': 'ts'
-				},
-				minify: true
+				minify: options.minify
 			}),
 			nodeResolve({ preferBuiltins: true, browser: false }),
 			(commonjs.default ?? commonjs)()
